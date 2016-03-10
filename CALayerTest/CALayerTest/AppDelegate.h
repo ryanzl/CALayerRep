90cbde7d70b4cc935e488e0299d5293164aa0919
killerggf <killerggf@126.com>
2016-03-10 13:59:45 +0800

    no message

---
+++ CALayerTest/CALayerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CALayerTest
+//
+//  Created by ryan on 16/3/10.
+//  Copyright © 2016年 ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CALayerTest/CALayerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CALayerTest
+//
+//  Created by ryan on 16/3/10.
+//  Copyright © 2016年 ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
